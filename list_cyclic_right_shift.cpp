702772622bef3b7f0e3e346715643e11e1af88c5
adam <asoliyev13@gmail.com>
2020-08-29 10:08:45 -0400

    Updated LF

--- list_cyclic_right_shift.cpp
+++ list_cyclic_right_shift.cpp
@@ -1,32 +1,9 @@
-"//List cyclic right shift" 
+#include <cmath>
 
+using namespace std;
 
-shared_ptr<ListNode<int>> CyclicallyRightShiftList(shared_ptr<ListNode<int>> L,
-                                                   int k) {
-  // TODO - you fill in here.
-    if (L == nullptr) {
-        return L;
-    }
-
-    // Compute the length of L and the tail.
-    auto tail = L;
-    int n = 1;
-    while (tail->next) {
-        ++n, tail = tail->next;
-    }
-    k %= n;
-    if (k == 0) {
-        return L;
-    }
-
-    tail->next = L;
-    int steps_to_new_head = n - k;
-    auto new_tail = tail;
-    while (steps_to_new_head--) {
-        new_tail = new_tail->next;
-    }
-    auto new_head = new_tail->next;
-    new_tail->next = nullptr;
-    return new_head;
+int main() {
+	int A = 12;
+	cout << pow(A, 3); << endl;
+	return 0;
 }
-
